ae2055195a4c7f3fcde809c98e0e02753fed31b7
VasokKiselicya <v.kiselicya021036@gmail.com>
2018-04-15 19:32:47 +0300

    Add Info Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,8 +2,15 @@ from rest_framework import serializers
 from api.models import Car
 
 
-class CarSerializer(serializers.HyperlinkedModelSerializer):
+class CarSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Car
         exclude = ()
+
+
+class ShortCarInfoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Car
+        fields = ("id", "name", "price", "volume", "city", "drive_type", "fuel", "transmission", "image")
